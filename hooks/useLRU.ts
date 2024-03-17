243e59a6f52567ea1937672a3b5e190e08eda597
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-03-17 12:26:33 +0530

    updating LRU logic

--- hooks/useLRU.ts
+++ hooks/useLRU.ts
@@ -1,68 +1,45 @@
+//@ts-nocheck
+import { ProductType } from "@/@types";
 import { create } from "zustand";
-
-export interface CacheItem {
-  key: string;
-  value: any;
-}
-
-class LRUCache {
-  capacity: number;
-  cache: CacheItem[];
-
-  constructor(capacity: number) {
-    this.capacity = capacity;
-    this.cache = [];
-  }
-
-  isCacheFull() {
-    return this.cache.length === this.capacity;
-  }
-
-  setItem(key: string, value: any) {
-    if (this.isCacheFull()) {
-      this.cache.pop();
-    }
-    this.cache.unshift({ key, value });
-  }
-
-  getItem(key: string) {
-    const cacheItem = this.cache.find((item) => item.key === key);
-    if (cacheItem) {
-      // Move the found item to the front
-      this.cache = this.cache.filter((item) => item.key !== key); // Remove the item
-      this.cache.unshift(cacheItem); // Move it to the front
-      return cacheItem.value;
-    }
-    return undefined;
-  }
+interface CacheItem {
+  id: string;
+  value: ProductType;
 }
 
 interface LRUStore {
   cache: CacheItem[];
-  setItem: (key: string, value: any) => void;
-  getItem: (key: string) => any;
+  setItem: (id: string, value: any) => void;
+  getItem: (id: string) => ProductType;
 }
 
-const useLRU = create<LRUStore>((set) => {
-  const totalCache = new LRUCache(5);
+const useLRU = create<LRUStore>((set) => ({
+  cache: [],
+  setItem: (id: string, value: any) => {
+    set((state) => {
+      const existingIndex = state.cache.findIndex((item) => item.id === id);
+      if (existingIndex !== -1) {
+        state.cache.unshift(state.cache.splice(existingIndex, 1)[0]);
+        state.cache[0].value = value; // Update value
+      } else {
+        if (state.cache.length >= 5) {
+          state.cache.pop();
+        }
 
-  return {
-    cache: [...totalCache.cache], // Return a copy of the cache array
-    setItem: (key: string, value: any) => {
-      const existingIndex = totalCache.cache.findIndex(
-        (item) => item.key === key
-      );
-
-      // If key not found, add the item
-      if (existingIndex === -1) totalCache.setItem(key, value);
-      else totalCache.cache[existingIndex].value = value;
-
-      set({ cache: [...totalCache.cache] });
-    },
-    getItem: (key: string) => {
-      return totalCache.getItem(key);
-    },
-  };
-});
+        state.cache.unshift({ id, value });
+      }
+      return { cache: [...state.cache] };
+    });
+  },
+  getItem: (id: string) => {
+    const itemIndex = state.cache.findIndex((item) => item.id === id);
+    if (itemIndex !== -1) {
+      // Move the item to the front
+      const [item] = state.cache.splice(itemIndex, 1);
+      state.cache.unshift(item);
+      return item.value;
+    }
+    return undefined;
+  },
+}));
 
 export default useLRU;
