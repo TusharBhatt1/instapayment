0a704e74b706d467a4f442d1778b81ecc3019506
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-03-19 00:05:19 +0530

    fix : unnecessary rerenders

--- components/RecentlyViewed.tsx
+++ components/RecentlyViewed.tsx
@@ -1,6 +1,6 @@
 "use client";
 import useLRU from "@/hooks/useLRU";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Heading from "./Heading";
 import ProductCard from "./ProductCard";
 import { usePathname } from "next/navigation";
@@ -10,28 +10,32 @@ export default function RecentlyViewed() {
   const [showFiltered, setShowFiltered] = useState(false);
   const path = usePathname();
   const params = path.split("/");
-
   const recentCacheId = params[params.length - 1];
-  if (recentCacheId === cache[0]?.id) {
-    setShowFiltered(true);
-    const filteredCache = cache.filter((cache) => cache.id !== recentCacheId);
-  } else setShowFiltered(false);
+
+  useEffect(() => {
+    if (recentCacheId === cache[0]?.id) {
+      setShowFiltered(true);
+    } else {
+      setShowFiltered(false);
+    }
+  }, [cache, recentCacheId]);
 
   let displayedCache = showFiltered
     ? cache.filter((cacheItem) => cacheItem.id !== recentCacheId)
     : cache;
+
   if (cache.length > 0) {
     return (
       <div className="flex flex-col gap-4 px-12 mb-7">
         <Heading title="Recently Viewed" />
         <div className="flex items-center flex-wrap gap-12">
           {displayedCache.map((product) => (
-            <div>
-              <ProductCard key={product.id} product={product.value} />
+            <div key={product.id}>
+              <ProductCard product={product.value} />
             </div>
           ))}
         </div>
       </div>
     );
-  }
+  } 
 }
