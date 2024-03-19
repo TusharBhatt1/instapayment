6a095c16074c6cb656a0e7286f70d31edb4ef5c3
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-03-20 00:25:32 +0530

    fix : typo

--- Others/Icons.ts
+++ Others/Icons.ts
@@ -5,10 +5,10 @@ import icon4 from "@/public/icons/4.webp";
 import icon5 from "@/public/icons/5.webp";
 
 export const icons = [
-  { label: "Accesories", image: icon1 },
+  { label: "Accessories", image: icon1 },
   { label: "Wallet", image: icon2 },
   { label: "Hoodies", image: icon3 },
-  { label: "Googles", image: icon4 },
+  { label: "Googgles", image: icon4 },
   { label: "MTX X", image: icon5 },
 
 ];
