0342d7517c4bf0b53adcab07cdb42f7dfd4f2d69
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-03-20 00:20:43 +0530

    cart : increase margin top

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -25,7 +25,7 @@ export default function page() {
     );
   return (
     <div>
-      <p className="text-center text-xl">Your Cart</p>
+      <p className="text-center text-xl mt-2">Your Cart</p>
     <div className="flex flex-col sm:flex-row justify-between  py-7 px-8 md:px-32 gap-12">
      <OrderList cart={cart}/>
      
